b6040db6b8b89f7cac31bf6a79287e63e54edcbe
Oswaldo Guevara <waldogg226@gmail.com>
2024-12-31 09:30:28 -0300

    first commit

---
+++ src/main/java/com/alura/litelalura/LitelaluraApplication.java
@@ -0,0 +1,13 @@
+package com.alura.litelalura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LitelaluraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LitelaluraApplication.class, args);
+	}
+
+}
